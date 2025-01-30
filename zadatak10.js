50eaa6cea1ca6b101ccd5ac78b5efb58413ae6ff
eminamumic <emimumic1@gmail.com>
2025-01-30 16:50:16 +0100

    gotov

--- zadatak10.js
+++ zadatak10.js
@@ -10,19 +10,16 @@ console.log(jeAritmetickiNiz([3, 6, 9, 15]));
 	true
 false
 */
-
 let jeAritmetickiNiz = (niz) => {
-  let provjera = niz[1] - niz[0]
-  niz.forEach((element, index) => {
-    if (index >= 1) {
-      if (provjera !== niz[index] - niz[index - 1]) {
-        return false
-      }
-      return true
+  let razlika = niz[1] - niz[0]
+  let konacno = niz.reduce((validator, element, index) => {
+    if (index === 0) return true
+    if (element - niz[index - 1] !== razlika) {
+      validator = false
+      return validator
     }
-  })
+    return validator
+  }, true)
+  return konacno
 }
-
-console.log(jeAritmetickiNiz([2, 4, 6, 8, 10]))
-
-//NIJE ZAVRSEN
+console.log(jeAritmetickiNiz([3, 6, 9, 15]))
